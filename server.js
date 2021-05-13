0b43179b7af3b95abb4073f455b46cb3b3e1becf
paras594 <parasarora594@gmail.com>
2021-05-13 20:44:35 +0530

    test 4

--- server.js
+++ server.js
@@ -38,11 +38,13 @@ app.get("/api/test", (req, res) => {
 	});
 });
 
-app.use(express.static(path.join(__dirname, "/client/build/")));
-app.get("/", (req, res) => {
-	// console.log({ path: path.join(__dirname, "./client/build/index.html") });
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/build/")));
+	app.get("*", (req, res) => {
+		// console.log({ path: path.join(__dirname, "./client/build/index.html") });
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port: ${port}`));
