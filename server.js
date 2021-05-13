e105657f2c108495a64eed70e09df4b95f3d77c7
paras594 <parasarora594@gmail.com>
2021-05-13 21:16:47 +0530

    test 7

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
 
 	app.get("*", (req, res) => {
 		res.json({ success: true });
-		// res.sendFile(path.join(__dirname, "client", "build"));
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 	});
 }
 
