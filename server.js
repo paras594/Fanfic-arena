b7a12db7ec4a2bb58d9eeb7da83e370219fb99e5
paras594 <parasarora594@gmail.com>
2021-05-13 21:21:08 +0530

    test 9

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require("./config/db.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
@@ -39,8 +40,6 @@ app.get("/api/test", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/build/")));
-
 	app.get("*", (req, res) => {
 		// res.json({ success: true });
 		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
