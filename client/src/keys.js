f7f44fbcd224fb32890c8c852dacca627875d3a4
paras594 <parasarora594@gmail.com>
2021-05-14 11:50:51 +0530

    test 23

--- client/src/keys.js
+++ client/src/keys.js
@@ -1,6 +1,6 @@
 const keys = {
-	// apiEndpoint: "https://fanfic-arena.herokuapp.com",
-	apiEndpoint: "http://localhost:5000",
+	apiEndpoint: "https://fanfic-arena.herokuapp.com",
+	// apiEndpoint: "http://localhost:5000",
 };
 
 export default keys;
